fbfd6937fcf94b2b223c1b7956f897a93c97438d
KHAYAN MALANTRUCCO <khayanmalantrucco@gmail.com>
2018-12-02 18:04:00 +0000

    Adicionada a categoria COUNTRY no BD.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,6 @@ rock.image.attach(io: File.open("#{tmp_dir}/seed_files/rock.png"), filename: "ro
 
 blues = Category.create(name: "Blues")
 blues.image.attach(io: File.open("#{tmp_dir}/seed_files/blues.png"), filename: "blues.png")
+
+country = Category.create(name: "Country")
+country.image.attach(io: File.open("#{tmp_dir}/seed_files/country.png"), filename: "country.png")
