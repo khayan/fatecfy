312044dab8a128ed0d254d0240b7bb2fae011d8a
KHAYAN MALANTRUCCO <khayanmalantrucco@gmail.com>
2018-12-02 18:18:48 +0000

    Adicionado álbum The Great Bear + 3 músicas.

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,14 +30,26 @@ jazz.image.attach(io: File.open("#{tmp_dir}/seed_files/jazz.png"), filename: "ja
 andrew_howes = Artist.create(name: "Andrew Howes")
 andrew_howes.photo.attach(io: File.open("#{tmp_dir}/seed_files/andrew_howes.jpg"), filename: "andrew_howes.jpg")
 
-gubernator = andrew_howes.albums.create(title: "Gubernator", date: Time.strptime("18/05/2015", "%d/%m/%Y"), category: rock)
-gubernator.cover.attach(io: File.open("#{tmp_dir}/seed_files/gubernator.jpg"), filename: "gubernator.jpg")
+    # ÁLBUM - GUBERNATOR
+    gubernator = andrew_howes.albums.create(title: "Gubernator", date: Time.strptime("18/05/2015", "%d/%m/%Y"), category: rock)
+    gubernator.cover.attach(io: File.open("#{tmp_dir}/seed_files/gubernator.jpg"), filename: "gubernator.jpg")
 
-dumb_luck = gubernator.songs.create(title: "Dumb Luck", played_count: Random.rand(1000))
-dumb_luck.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/dumb_luck.mp3"), filename: "dumb_luck.mp3")
-helmsman = gubernator.songs.create(title: "Helmsman", played_count: Random.rand(1000))
-helmsman.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/helmsman.mp3"), filename: "helmsman.mp3")
-crimea = gubernator.songs.create(title: "Crimea", played_count: Random.rand(1000))
-crimea.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/crimea.mp3"), filename: "crimea.mp3")
-traitors_gate = gubernator.songs.create(title: "Traitors Gate", played_count: Random.rand(1000))
-traitors_gate.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/traitors_gate.mp3"), filename: "traitors_gate.mp3")
+    dumb_luck = gubernator.songs.create(title: "Dumb Luck", played_count: Random.rand(1000))
+    dumb_luck.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/dumb_luck.mp3"), filename: "dumb_luck.mp3")
+    helmsman = gubernator.songs.create(title: "Helmsman", played_count: Random.rand(1000))
+    helmsman.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/helmsman.mp3"), filename: "helmsman.mp3")
+    crimea = gubernator.songs.create(title: "Crimea", played_count: Random.rand(1000))
+    crimea.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/crimea.mp3"), filename: "crimea.mp3")
+    traitors_gate = gubernator.songs.create(title: "Traitors Gate", played_count: Random.rand(1000))
+    traitors_gate.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/traitors_gate.mp3"), filename: "traitors_gate.mp3")
+    
+    # ÁLBUM - THE GREAT BEAR
+    the_great_bear = andrew_howes.albums.create(title: "The Great Bear", date: Time.strptime("19/05/2015", "%d/%m/%Y"), category: rock)
+    the_great_bear.cover.attach(io: File.open("#{tmp_dir}/seed_files/the_great_bear.jpg"), filename: "the_great_bear.jpg")
+     
+    big_drop = the_great_bear.songs.create(title: "Big Drop", played_count: Random.rand(1000))
+    big_drop.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/big_drop.mp3"), filename: "big_drop.mp3")
+    four_am = the_great_bear.songs.create(title: "4am", played_count: Random.rand(1000))
+    four_am.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/4_am.mp3"), filename: "4_am.mp3")
+    waiting = the_great_bear.songs.create(title: "Waiting", played_count: Random.rand(1000))
+    waiting.file.attach(io: File.open("#{tmp_dir}/seed_files/musics/waiting.mp3"), filename: "waiting.mp3")
