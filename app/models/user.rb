785736a4a586b2bc13fcd9cba3b6e9602a144e80
KHAYAN MALANTRUCCO <khayanmalantrucco@gmail.com>
2018-12-02 16:50:01 +0000

    Ajuste no código do MODEL USER. Adicionado comentários sobre outros módulos disponíveis.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Módulos padrão do DEVISE.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Outros módulos disponíveis:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
          
   has_many :favorites
   has_many :recently_heards
