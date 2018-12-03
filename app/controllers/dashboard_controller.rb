53b9e2858420c9353569a2e27b5ddf887f7fdada
KHAYAN MALANTRUCCO <khayanmalantrucco@gmail.com>
2018-12-03 11:37:59 +0000

    CONTROLLER DASHBOARD modificado. Inserido método RECENT_HEARD.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    load_recent_heard
+    load_recommendations
+  end
+  
+  def load_recent_heard
+    @recent_albums = current_user.recently_heards.order("created_at DESC").limit(4).map(&:album)
   end
 end
